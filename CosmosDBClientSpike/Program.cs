23e203131e8939b8850aef681cf9ebbff3c0a0d0
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-06-27 14:36:47 +0900

    Add project files.

---
+++ CosmosDBClientSpike/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CosmosDBClientSpike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
